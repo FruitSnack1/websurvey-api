54951835fb02fede331189293412912377bb1b26
unknown <tomas.ryzner@email.cz>
2020-02-03 16:14:06 +0100

    mongoose

--- app.js
+++ app.js
@@ -1,51 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-// const bodyParser =  require('body-parser');
-var indexRouter = require('./routes/index');
-const users  = require('./routes/users');
-const ankety = require('./routes/ankety');
-const play = require('./routes/play');
-const results = require('./routes/results');
+require('dotenv').config()
 
-var fileUpload = require("express-fileupload");
-var mongo = require('mongodb');
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
 
-var app = express();
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
-app.use(fileUpload());
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.json())
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyParser.urlencoded())
-app.use('/', indexRouter);
-app.use('/users', users);
-app.use('/ankety', ankety);
-app.use('/play', play);
-app.use('/results', results);
+// const subscribersRouter = require('./routes/subscribers')
+// app.use('/subscribers', subscribersRouter)
+const userRouter = require('./routes/users')
+app.use('/users', userRouter)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(3000)
--- routes/users.js
+++ routes/users.js
@@ -1,120 +1,136 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
-const url = 'mongodb://127.0.0.1:27017/quiz';
-const jsonexport = require('jsonexport');
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const url = 'mongodb://127.0.0.1:27017/quiz';
+// const jwt = require('jsonwebtoken');
+// const crypto = require('crypto');
 
-router.get('/protected', authenticateToken, (req,res)=>{
-  res.send('you re good');
-});
+// router.get('/protected', authenticateToken, (req,res)=>{
+//   res.send('you re good');
+// });
 
-router.get('/users', (req, res) => {
-  res.send({
-    'count': 1
-  })
-});
+// router.get('/users', (req, res) => {
+//   res.send({
+//     'count': 1
+//   })
+// });
 
-router.get('/login', (req, res) => {
-  MongoClient.connect(url, async (err, client) => {
-    if (err) return console.log('Unable to connect to the Server', err);
-    const db = client.db("quiz");
-    const users = await db.collection('users').find({}, {
-      projection: {
-        password: 0
-      }
-    }).toArray();
-    res.render('login', {
-      users
-    });
-  });
-});
+// router.get('/login', (req, res) => {
+//   MongoClient.connect(url, async (err, client) => {
+//     if (err) return console.log('Unable to connect to the Server', err);
+//     const db = client.db("quiz");
+//     const users = await db.collection('users').find({}, {
+//       projection: {
+//         password: 0
+//       }
+//     }).toArray();
+//     res.render('login', {
+//       users
+//     });
+//   });
+// });
 
-router.post('/login', (req,res) =>{
-  const id = mongodb.ObjectId(req.body.id);
-  const password = req.body.password;
+// router.post('/login', (req,res) =>{
+//   const id = mongodb.ObjectId(req.body.id);
+//   const password = req.body.password;
 
-  const hash = crypto.createHash('md5').update(password).digest('hex');
+//   const hash = crypto.createHash('md5').update(password).digest('hex');
 
-  MongoClient.connect(url, async (err, client) => {
-    if (err) return console.log('Unable to connect to the Server', err);
-    const db = client.db("quiz");
-    await db.collection('users').find({'_id': id}).toArray((err, result) =>{
-      if(err) console.log(err);
-      const dbUser = result[0];
-      if(hash === dbUser.password){
+//   MongoClient.connect(url, async (err, client) => {
+//     if (err) return console.log('Unable to connect to the Server', err);
+//     const db = client.db("quiz");
+//     await db.collection('users').find({'_id': id}).toArray((err, result) =>{
+//       if(err) console.log(err);
+//       const dbUser = result[0];
+//       if(hash === dbUser.password){
 
-        const user = { 'id' : dbUser._id, 'username': dbUser.username };
-        const accessToken = generateAccessToken(user);
-        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+//         const user = { 'id' : dbUser._id, 'username': dbUser.username };
+//         const accessToken = generateAccessToken(user);
+//         const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
 
-        //save refreshToken
+//         //save refreshToken
 
-        res.cookie('accessToken', accessToken);
-        res.cookie('refreshToken', refreshToken);
-        res.redirect('/admin')
-      }else{
-        res.send('wrong password');
-      }
-    });
-  });
-});
+//         res.cookie('accessToken', accessToken);
+//         res.cookie('refreshToken', refreshToken);
+//         res.redirect('/admin')
+//       }else{
+//         res.send('wrong password');
+//       }
+//     });
+//   });
+// });
 
-router.get('/register', (req, res) => {
-  res.render('register');
-});
+// router.get('/register', (req, res) => {
+//   res.render('register');
+// });
 
-router.post('/register', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const confirmed_password = req.body.confirmed_password;
+// router.post('/register', (req, res) => {
+//   const username = req.body.username;
+//   const password = req.body.password;
+//   const confirmed_password = req.body.confirmed_password;
 
-  const hash = crypto.createHash('md5').update(password).digest('hex');
+//   const hash = crypto.createHash('md5').update(password).digest('hex');
 
-  MongoClient.connect(url, async (err, client) => {
-    if (err) return console.log('Unable to connect to the Server', err);
-    const db = client.db("quiz");
-    db.collection('users').insertOne(
-      {
-        'username' : username,
-        'password' : hash
-      }
-    );
-    res.redirect('/users/login');
-  });
-});
+//   MongoClient.connect(url, async (err, client) => {
+//     if (err) return console.log('Unable to connect to the Server', err);
+//     const db = client.db("quiz");
+//     db.collection('users').insertOne(
+//       {
+//         'username' : username,
+//         'password' : hash
+//       }
+//     );
+//     res.redirect('/users/login');
+//   });
+// });
 
 
-function getUser(id) {
-  MongoClient.connect(url, async (err, client) => {
-    if (err) return console.log('Unable to connect to the Server', err);
-    const db = client.db("quiz");
-    const user = await db.collection(`users`).find({
-      '_id': mongodb.ObjectId(id)
-    });
-    return user;
-  });
-}
+// function getUser(id) {
+//   MongoClient.connect(url, async (err, client) => {
+//     if (err) return console.log('Unable to connect to the Server', err);
+//     const db = client.db("quiz");
+//     const user = await db.collection(`users`).find({
+//       '_id': mongodb.ObjectId(id)
+//     });
+//     return user;
+//   });
+// }
 
-function generateAccessToken(data) {
-  return jwt.sign(data, process.env.ACCESS_TOKEN_SECRET)
-}
+// function generateAccessToken(data) {
+//   return jwt.sign(data, process.env.ACCESS_TOKEN_SECRET)
+// }
 
-function authenticateToken(req, res, next) {
-  const token = req.cookies['accessToken'];
-  if (token == null) return res.sendStatus(401)
+// function authenticateToken(req, res, next) {
+//   const token = req.cookies['accessToken'];
+//   if (token == null) return res.sendStatus(401)
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
-}
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//     console.log(err)
+//     if (err) return res.sendStatus(403)
+//     req.user = user
+//     next()
+//   })
+// }
 
-module.exports = router;
+// module.exports = router;
+
+
+const express = require('express')
+const router = express.Router()
+
+const User = require('../models/user')
+
+router.get('/', async (req,res)=>{
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+module.exports = router
